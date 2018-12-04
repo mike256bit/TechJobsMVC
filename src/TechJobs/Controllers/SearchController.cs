ea967fca48b47a98c196fd59449fd37a690955d6
mike256bit <mike.toth@gmail.com>
2018-12-03 19:30:28 -0500

    Updated TechJobsMVC per assignment

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -2,19 +2,39 @@
 using Microsoft.AspNetCore.Mvc;
 using TechJobs.Models;
 
+
 namespace TechJobs.Controllers
 {
     public class SearchController : Controller
     {
         public IActionResult Index()
         {
+            ViewBag.error = "";
             ViewBag.columns = ListController.columnChoices;
             ViewBag.title = "Search";
             return View();
         }
 
-        // TODO #1 - Create a Results action method to process 
-        // search request and display results
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            if (searchTerm != null && searchType.ToLower() != "all")
+            {
+                List<Dictionary<string, string>> jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.jobs = jobs;
+            }
+            else if (searchTerm != null)
+            {
+                List<Dictionary<string, string>> jobs = JobData.FindByValue(searchTerm);
+                ViewBag.jobs = jobs;
+            }
+            else
+            {
+                ViewBag.error = "Please provide a search term.";
+            }
+            
+            ViewBag.columns = ListController.columnChoices;
+            return View("Index");
+        }
 
     }
 }
